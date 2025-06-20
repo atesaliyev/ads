4832995fb238526e3f21f91f23bd419903909761
atesaliyev <orgvesta@gmail.com>
2025-06-20 17:31:47 +0300

    go

--- config_reader.py
+++ config_reader.py
@@ -7,57 +7,52 @@ from logger import logger
 
 
 @dataclass
-class PathParams:
+class GeneralParams:
     query_file: str
-    proxy_file: str
-    user_agents: Optional[str] = "user_agents.txt"
-    filtered_domains: Optional[str] = "domains.txt"
+    domains: str
+    domain_mapping: str
+    multi_browser_in_use: bool
+    run_on_startup: bool
 
 
 @dataclass
 class WebdriverParams:
-    proxy: str
-    auth: Optional[bool] = False
-    incognito: Optional[bool] = False
-    country_domain: Optional[bool] = False
-    language_from_proxy: Optional[bool] = False
-    ss_on_exception: Optional[bool] = False
-    window_size: Optional[str] = ""
-    shift_windows: Optional[bool] = False
-    use_seleniumbase: Optional[bool] = False
+    browser: str
+    incognito: bool
+    auth: bool
+    proxy_file: Optional[str]
+    country_domain: bool
+    language_from_proxy: bool
+    use_seleniumbase: bool
+    window_size: Optional[str]
+    shift_windows: bool
+    check_shopping_ads: bool
 
 
 @dataclass
 class BehaviorParams:
-    query: str
-    ad_page_min_wait: Optional[int] = 10
-    ad_page_max_wait: Optional[int] = 15
-    nonad_page_min_wait: Optional[int] = 15
-    nonad_page_max_wait: Optional[int] = 20
-    max_scroll_limit: Optional[int] = 0
-    check_shopping_ads: Optional[bool] = True
-    excludes: Optional[str] = ""
-    random_mouse: Optional[bool] = False
-    custom_cookies: Optional[bool] = False
-    click_order: Optional[int] = 5
-    browser_count: Optional[int] = 2
-    multiprocess_style: Optional[int] = 1
-    loop_wait_time: Optional[int] = 60
-    wait_factor: Optional[float] = 1.0
-    running_interval_start: Optional[str] = ""
-    running_interval_end: Optional[str] = ""
-    twocaptcha_apikey: Optional[str] = ""
-    hooks_enabled: Optional[bool] = False
-    telegram_enabled: Optional[bool] = False
-    send_to_android: Optional[bool] = False
-    request_boost: Optional[bool] = False
+    ad_click_probability: float
+    max_ad_clicks_per_query: int
+    max_non_ad_clicks_per_query: int
+    max_shopping_ad_clicks_per_query: int
+    max_total_clicks_per_query: int
+    wait_factor: float
+    ad_page_min_wait: int
+    ad_page_max_wait: int
+    nonad_page_min_wait: int
+    nonad_page_max_wait: int
+    random_mouse: bool
+    custom_cookies: bool
+    delete_cookies: bool
+    hooks_enabled: bool
+    twocaptcha_apikey: str
 
 
 class ConfigReader:
     """Config file reader"""
 
     def __init__(self) -> None:
-        self.paths = None
+        self.general = None
         self.webdriver = None
         self.behavior = None
 
@@ -71,58 +66,43 @@ class ConfigReader:
                 logger.error("Failed to read config file. Check format and try again.")
                 raise SystemExit()
 
-        self.paths = PathParams(
-            query_file=config["paths"]["query_file"],
-            proxy_file=config["paths"]["proxy_file"],
-            user_agents=config["paths"]["user_agents"],
-            filtered_domains=config["paths"]["filtered_domains"],
+        self.general = GeneralParams(
+            query_file=config["general"]["query_file"],
+            domains=config["general"]["domains"],
+            domain_mapping=config["general"]["domain_mapping"],
+            multi_browser_in_use=config["general"]["multi_browser_in_use"],
+            run_on_startup=config["general"]["run_on_startup"],
         )
-
-        if self.paths.proxy_file and config["webdriver"]["proxy"]:
-            logger.error("Either 'proxy_file' or 'proxy' parameter should be empty.")
-            raise SystemExit()
 
         self.webdriver = WebdriverParams(
-            proxy=config["webdriver"]["proxy"],
-            auth=config["webdriver"]["auth"],
+            browser=config["webdriver"]["browser"],
             incognito=config["webdriver"]["incognito"],
+            auth=config["webdriver"]["auth"],
+            proxy_file=config["webdriver"]["proxy_file"],
             country_domain=config["webdriver"]["country_domain"],
             language_from_proxy=config["webdriver"]["language_from_proxy"],
-            ss_on_exception=config["webdriver"]["ss_on_exception"],
+            use_seleniumbase=config["webdriver"]["use_seleniumbase"],
             window_size=config["webdriver"]["window_size"],
             shift_windows=config["webdriver"]["shift_windows"],
-            use_seleniumbase=config["webdriver"]["use_seleniumbase"],
+            check_shopping_ads=config["webdriver"]["check_shopping_ads"],
         )
 
-        if self.paths.query_file and config["behavior"]["query"]:
-            logger.error("Either 'query_file' or 'query' parameter should be empty.")
-            raise SystemExit()
-
-        browser_count = config["behavior"]["browser_count"]
-
         self.behavior = BehaviorParams(
-            query=config["behavior"]["query"],
+            ad_click_probability=config["behavior"]["ad_click_probability"],
+            max_ad_clicks_per_query=config["behavior"]["max_ad_clicks_per_query"],
+            max_non_ad_clicks_per_query=config["behavior"]["max_non_ad_clicks_per_query"],
+            max_shopping_ad_clicks_per_query=config["behavior"]["max_shopping_ad_clicks_per_query"],
+            max_total_clicks_per_query=config["behavior"]["max_total_clicks_per_query"],
+            wait_factor=config["behavior"]["wait_factor"],
             ad_page_min_wait=config["behavior"]["ad_page_min_wait"],
             ad_page_max_wait=config["behavior"]["ad_page_max_wait"],
             nonad_page_min_wait=config["behavior"]["nonad_page_min_wait"],
             nonad_page_max_wait=config["behavior"]["nonad_page_max_wait"],
-            max_scroll_limit=config["behavior"]["max_scroll_limit"],
-            check_shopping_ads=config["behavior"]["check_shopping_ads"],
-            excludes=config["behavior"]["excludes"],
             random_mouse=config["behavior"]["random_mouse"],
             custom_cookies=config["behavior"]["custom_cookies"],
-            click_order=config["behavior"]["click_order"],
-            browser_count=multiprocessing.cpu_count() if browser_count == 0 else browser_count,
-            multiprocess_style=config["behavior"]["multiprocess_style"],
-            loop_wait_time=config["behavior"]["loop_wait_time"],
-            wait_factor=config["behavior"]["wait_factor"],
-            running_interval_start=config["behavior"]["running_interval_start"],
-            running_interval_end=config["behavior"]["running_interval_end"],
-            twocaptcha_apikey=config["behavior"]["2captcha_apikey"],
+            delete_cookies=config["behavior"]["delete_cookies"],
             hooks_enabled=config["behavior"]["hooks_enabled"],
-            telegram_enabled=config["behavior"]["telegram_enabled"],
-            send_to_android=config["behavior"]["send_to_android"],
-            request_boost=config["behavior"]["request_boost"],
+            twocaptcha_apikey=config["behavior"]["twocaptcha_apikey"],
         )
 
 
